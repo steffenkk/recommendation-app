ef6088bc746113dee99f05aff07a54bc062b5eb0
Steffen Klempau <Steffen.Klempau@Fielmann.com>
2020-12-14 21:03:19 +0100

    dependencies + python packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="recommendation_app",
+    version="0.0.1",
+    author="Steffem Klempau",
+    author_email="steffen.klempau@yahoo.de",
+    description="Recommendation system with REST endpoint",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/steffenkk/recommendation-app",
+    package_dir={"recommendation_app": "src"},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
