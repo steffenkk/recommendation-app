815f7ecf6932f7ae5a96f8167023ca144c525788
Steffen Klempau <Steffen.Klempau@Fielmann.com>
2020-12-17 07:55:47 +0100

    Params from Payload

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from typing import Optional, List, Dict
+from typing import Optional, Dict
 from fastapi import FastAPI
 from src.user import User
 
@@ -10,13 +10,18 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/items/")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/recommendation/{user_id}/{order_list}")
-async def get_recommendation(user_id: int, order_list: List[Dict[str:int]]):
-    user = User(id=user_id, orders=order_list)
+@app.get("/recommendations/")
+def get_recommendation(user_id: int, orders: Optional[Dict[str, float]] = None):
+    user = User(id=user_id, orders=orders)
     user.set_order_df()
-    return user.get_recommendation(number_ptions=5)
+    recommendations = user.get_recommendation(number_ptions=5)
+    return {
+        "user_id": user_id,
+        "past_orders": orders,
+        "recommendations": recommendations,
+    }
