2d5999df381b5951d4b2123570d3fc033e4b8e3b
Steffen Klempau <Steffen.Klempau@Fielmann.com>
2020-12-19 16:26:55 +0100

    recommender class

--- src/recommender.py
+++ src/recommender.py
@@ -1,62 +1,62 @@
-# import random
-
 import pandas as pd
-from toolz import curry, pipe
+from pydantic import BaseModel
 
-from src.helpers import read_csv
-from src.prepare import get_user_orders
+from typing import Dict
+
+from src.cache import CachedData
+from src.user import User
 
 # Actual Recommendation
-# TODO: try to predict purchases to evaluate params
-# TODO: refactor and write tests
 
 
-def generate_recommended_products(df: pd.DataFrame, user_orders: pd.DataFrame):
-    """retrieve recommendations for the user's articles"""
-    products = [
-        df.get(ind).dropna().map(lambda x: x * user_orders.loc[ind, :][0])
-        for ind in user_orders.index
-    ]
-    return pd.concat(products).sort_values(ascending=False)
+class Recommender(BaseModel):
+    data: CachedData
+    user: User
+    recommender_df: pd.DataFrame = None
+    recommended_products: Dict = None
 
+    class Config:
+        """ whether or not to allow custom types """
 
-@curry
-def remove_already_bought(user_orders: pd.Series, df: pd.DataFrame):
-    return df.drop(user_orders.index, axis=0, errors="ignore")
+        arbitrary_types_allowed = True
 
+    def _set_recommender_df(self):
+        """retrieve recommendations for the user's articles"""
+        sim_matrix = self.data.get_sim_matrix()
+        products = [
+            sim_matrix.get(ind)
+            .dropna()
+            .map(lambda x: x * self.user.orderDF.loc[ind, :][0])
+            for ind in self.user.orderDF.index
+        ]
+        self.recommender_df = pd.concat(products).sort_values(ascending=False)
 
-@curry
-def remove_duplicates(df: pd.DataFrame):
-    """ group by the dataframes index """
-    return df.groupby(level=0).sum().sort_values(ascending=False)
+    def _set_recommended_products(self, number_options: int):
+        """set recommendet products as dict to be serializable"""
+        self.recommended_products = (
+            self.recommender_df.round(2).head(number_options).to_dict()
+        )
 
+    def _remove_already_bought(self):
+        """ avoid recommending already bought products """
+        self.recommender_df = self.recommender_df.drop(
+            self.user.orderDF.index, axis=0, errors="ignore"
+        )
 
-def recommend(df: pd.DataFrame, user_orders: pd.DataFrame, item_field: str):
-    """This function will be invoked by API Call -
-    then add remove administrative product here"""
-    return pipe(
-        generate_recommended_products(df, user_orders),
-        remove_already_bought(user_orders),
-        remove_duplicates(),
-    )
+    def _remove_duplicates(self):
+        """ group by the dataframes index """
+        self.recommender_df = (
+            self.recommender_df.groupby(level=0).sum().sort_values(ascending=False)
+        )
 
+    def get_recommendation(self, number_options: int):
+        """This function will be invoked by API Call"""
 
-def process(
-    user_orders: pd.DataFrame,
-    item_field: str = "Description",
-    number_options: int = 10,
-):
-    similiar_items = read_csv("../data/sim_matrix.csv")
-    similiar_items.set_index(similiar_items.columns[0], inplace=True)
-    return recommend(similiar_items, user_orders, item_field).head(number_options)
+        if not self.recommender_df:
+            self._set_recommender_df()
 
+        self._remove_already_bought()
+        self._remove_duplicates()
+        self._set_recommended_products(number_options=number_options)
 
-if __name__ == "__main__":
-    uid = 111  # random.randint(0, 3800)
-    user_orders = get_user_orders(uid=uid, item_field="Description")
-    user_orders.to_json(f"./users/{uid}.json", force_ascii=False)
-    print("\n--- The user ordered --- \n")
-    print(user_orders.head(30))
-    recommendation = process(user_orders)
-    print("\n --- We would recommend ---  \n")
-    print(recommendation)
+        return self.recommended_products
