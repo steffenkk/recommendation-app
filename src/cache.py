efb5e782cfe391d34fea1201e531ba1f8e60d028
Steffen Klempau <Steffen.Klempau@Fielmann.com>
2020-12-21 18:33:40 +0100

    allow bring "yor own sim matrix"

--- src/cache.py
+++ src/cache.py
@@ -1,10 +1,20 @@
+from typing import Optional
+from pandas import DataFrame
+
 from src.helpers import read_csv
 
 
 class CachedData:
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-        self._sim_matrix = self._read_sim_matrix()
+    def __init__(
+        self, file_path: Optional[str] = None, sim_matrix: Optional[DataFrame] = None
+    ):
+        if file_path is not None or sim_matrix is not None:
+            self.file_path = file_path
+            self.sim_matrix = (
+                sim_matrix if sim_matrix is not None else self._read_sim_matrix()
+            )
+        else:
+            raise AttributeError("One of file_path and sim_matrix must be provided ")
 
     def _read_sim_matrix(self):
         df = read_csv(self.file_path)
@@ -12,4 +22,4 @@ class CachedData:
         return df
 
     def get_sim_matrix(self):
-        return self._sim_matrix
+        return self.sim_matrix
